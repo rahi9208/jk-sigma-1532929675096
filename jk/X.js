b6037b4158b4aad8faf334d337d7a3e30616df93
rahi9208 <rahi@wmail.club>
2018-07-30 15:31:56 +0530

    update

--- jk/X.js
+++ jk/X.js
@@ -1,6 +1,6 @@
 let AWS = require('aws-sdk');
 
 exports.handler = function(event, context, callback) {
-    console.log("Hello Chathura Update");
+    
     callback(null,'Successfully executed');
 }
